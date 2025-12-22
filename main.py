68195ace63ac6f2a39fd7028bb8cd87a1e81c3f6
Mayoz <aayush.cs10@gmail.com>
2025-12-22 21:03:46 +0545

    Very first and simple version of chatbot

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-print('hello, I am a python chatbot')
-print('This is to check git')
-print('Does this work?')
-print('Added one more line')
+print('Hi! I\'m a simple chatbot')
+print('Type "exit" to stop')
+
+while True:
+  user_msg = input('You: ')
+
+  if user_msg.lower() == 'exit' :
+    print('Bot: Goodbye...')
+    break
+  elif user_msg.lower() == 'hi':
+    print('Bot: Hello! how are you?')
+  elif user_msg.lower() == 'how are you':
+    print('Bot: I am just a code, but I am fine')
+  else:
+    print('Bot: I didn\'t understand what you just said')
+    
