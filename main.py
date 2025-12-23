9942f5e56cd2ee85a14870be378f6198fc57bb5e
Mayoz <aayush.cs10@gmail.com>
2025-12-23 09:04:47 +0545

    Added dictionary for responses

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+responses = {
+  'hi':'Hello, how are you?',
+  'i am fine': 'That\'s good to know.',
+  'how are you': 'I am just a code, but I\'m doing great',
+  'good': 'Thankyou!',
+  'bye':'Goodbye, please type "exit" to quit'
+}
+
 print('Hi! I\'m a simple chatbot')
-print('Type "exit" to stop')
 
 while True:
-  user_msg = input('You: ')
+  user_msg = input('You: ').lower()
 
   if user_msg.lower() == 'exit' :
-    print('Bot: Goodbye...')
+    print('Bot: Goodbye, I\'m out...')
     break
-  elif user_msg.lower() == 'hi':
-    print('Bot: Hello! how are you?')
-  elif user_msg.lower() == 'how are you':
-    print('Bot: I am just a code, but I am fine')
-  else:
-    print('Bot: I didn\'t understand what you just said')
-    
+
+  reply = responses.get(user_msg, 'I don\'t understand')
+  print('Bot:',reply)
