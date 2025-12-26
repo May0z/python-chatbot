143e01e250ec95c41b0749299020ef42a45e219a
Mayoz <aayush.cs10@gmail.com>
2025-12-26 19:41:44 +0545

    Added dynamic replies depending upon intention

--- test.py
+++ test.py
@@ -11,32 +11,28 @@
 
 # shinobis = {"Naruto":"genin", "Sasuke":"genin", "Shikamaru":"chunin", "Kakashi":"chunin", "Tsunade":"Hokage", "Itachi":"rogue", "Orochimaru":"rogue"}
 
-# indents = {
-#   "ninja":["jutsu","shuriken","chakra"],
-#   "pirate":["devil fruit", "katana", "haki"],
-#   "saiyan":["martial arts", "android", "ki"]
-# }
-
-# responses = {
-#   "ninja":"Naruto",
-#   "pirate":"Luffy",
-#   "saiyan":"Goku"
-# }
-
-users = {
-  "user1": {"name":"Aayush", "job":"Loser", "skills":["Java","PHP"]},
-  "user2":{"name":"Rashmita", "job":"Asc. Developer", "skills":["Java","PHP","DevOps","SQL"]}
+indents = {
+  "ninja":["jutsu","shuriken","chakra"],
+  "pirate":["devil fruit", "katana", "haki"],
+  "saiyan":["martial arts", "android", "ki"]
 }
-users.update({"user3":{"name":"Sudeep", "job":"Loser", "skills":["SEO","SQL"]}})
-users["user2"]["skills"].append("PHP")
-users["user3"]["skills"].append("React")
 
-users["user1"]["skills"].remove("PHP")
-for user_id, user_details in users.items():
-  print(user_id)
-  print('Name:',user_details["name"])
-  print('Skills:')
-  for skill in user_details['skills']:
-    print('-', skill)
+responses = {
+  "ninja":"Naruto",
+  "pirate":"Luffy",
+  "saiyan":"Goku"
+}
 
-  print()
+user_input = input("Tell me what power/weapon/energy does you character use? ")
+
+detected_indent = None
+
+for indent, battle in indents.items():
+  if user_input in battle:
+    detected_indent = indent
+    break
+
+if detected_indent:
+  print("Your character is", responses.get(detected_indent))
+else:
+  print("Yout character is not available.")
